aaa824b441d3879a9f6e3912bc454a00d7bcb85f
Michael LeBlanc <leblanc.michael@gmail.com>
2023-10-04 06:09:03 -0400

    Add zsh

--- install.sh
+++ install.sh
@@ -6,3 +6,13 @@ for F in nvim/*; do
 	    ln -sf "$(pwd)/$F" "$HOME/.config/$F"
     fi
 done
+
+mkdir -p $HOME/.config/zsh
+ln -sf "$(pwd)/zsh/zshenv" "$HOME/.zshenv"
+cd zsh
+for F in *; do
+    if [[ -f "$F" ]]; then
+        ln -sf "$(pwd)/$F" "$HOME/.config/zsh/.$F"
+    fi
+done
+cd ..
